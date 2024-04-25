dc8a6d3e685e0d37f74a7e89694413746d306094
misaki.taneda <misaki.taneda.j847@dirbato.co.jp>
2024-04-25 16:08:31 +0900

    CH8終了

--- TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,16 +39,19 @@ public class UserController {
         return "user/register";
     }
     
-    //User登録処理
+    // User登録処理 
     @PostMapping("/register")
-    public String postRegister(User user) {
-        //User登録
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
+        // User登録
         service.saveUser(user);
-        //一覧画面にリダイレクト
+        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    
-    /** User更新画面を表示 */
+    // User更新画面を表示 
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
         // Modelに登録
@@ -55,7 +60,7 @@ public class UserController {
         return "user/update";
     }
 
-    /** User更新処理 */
+    // User更新処理
     @PostMapping("/update/{id}/")
     public String postUser(User user) {
         // User登録
